a9ba1da2c44d33583f12584f275582a955046fad
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-09-07 16:57:14 -0700

    build game screen shorts

---
+++ src/hooks/useScreenShots.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+import ApiClient from "../services/api-client"
+import { Screenshot } from "../entities/Screenshot"
+
+
+const useScreenShots = (gameId: number) =>
+{
+    const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`)
+
+    return  useQuery({
+    queryKey: ['screenshots', gameId],
+    queryFn:apiClient.getAll
+   })
+}
+    
+export default useScreenShots
+    
+   
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -6,6 +6,7 @@ import { Heading } from '@chakra-ui/react'
 import ExpandableText from '../components/ExpandableText'
 import GameAttributes from '../components/GameAttributes'
 import GameTrailer from '../components/GameTrailer'
+import GameScreenshots from '../components/GameScreenshots'
 
 
 const GameDetailsPage = () => {
@@ -18,7 +19,8 @@ const GameDetailsPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
-      <GameTrailer gameId={game.id}/>
+      <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id}/>
     </>
   )
 }

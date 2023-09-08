0d226348569c52598dce7a0ff03e1500cf5a9464
Mahima Mukhi <116036967+mmukhi00@users.noreply.github.com>
2023-09-07 17:03:13 -0700

    improve the layout

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -7,6 +7,8 @@ import ExpandableText from '../components/ExpandableText'
 import GameAttributes from '../components/GameAttributes'
 import GameTrailer from '../components/GameTrailer'
 import GameScreenshots from '../components/GameScreenshots'
+import { SimpleGrid } from '@chakra-ui/react'
+import { GridItem } from '@chakra-ui/react'
 
 
 const GameDetailsPage = () => {
@@ -15,13 +17,18 @@ const GameDetailsPage = () => {
   if (isLoading) return <Spinner />
   if (error || !game) throw error;
   return (
-    <>
+    <SimpleGrid columns={{ base: 1, ms: 2 }} spacing={5}>
+      <GridItem>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
       <GameScreenshots gameId={game.id}/>
-    </>
+     </GridItem>
+     
+    </ SimpleGrid>
   )
 }
 
